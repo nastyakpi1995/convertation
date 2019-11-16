23774d5454df24690f1c2565a1a2684523b470cf
Nastia Krat <nastyakpi1995@gmail.com>
2019-11-16 10:16:07 +0200

    Updates

--- precache-manifest.187ab2a4128f43efab2a9abb1aff9fdc.js
+++ precache-manifest.187ab2a4128f43efab2a9abb1aff9fdc.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "dfdc08cbcde0b99909897e61c457bcf0",
+    "revision": "a23e11e3b6b0417b7c89b98c698e1553",
     "url": "/convertation/index.html"
   },
   {
-    "revision": "68967ea2da607217550c",
+    "revision": "2f2eacf989758cfe890d",
     "url": "/convertation/static/css/main.9e894c54.chunk.css"
   },
   {
-    "revision": "b0f9b646d577d089d347",
-    "url": "/convertation/static/js/2.811f434d.chunk.js"
+    "revision": "5c145886e1bf3d22dfbb",
+    "url": "/convertation/static/js/2.b60450ac.chunk.js"
   },
   {
-    "revision": "68967ea2da607217550c",
-    "url": "/convertation/static/js/main.5c75abc3.chunk.js"
+    "revision": "2f2eacf989758cfe890d",
+    "url": "/convertation/static/js/main.e1e2230b.chunk.js"
   },
   {
     "revision": "5849f1a4ca3f5def4c07",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/convertation/precache-manifest.6ffb271ca426a0016d7bb5b5ad531ec0.js"
+  "/convertation/precache-manifest.187ab2a4128f43efab2a9abb1aff9fdc.js"
 );
 
 self.addEventListener('message', (event) => {
